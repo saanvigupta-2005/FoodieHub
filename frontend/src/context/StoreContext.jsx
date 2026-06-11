c58264da14d77352466a5923da374ba1840534ee
shiksha101 <shikshadahiya1710@gmail.com>
2026-06-11 10:50:43 +0530

    explorer menu

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-export const StoreContext = React.createContext(null)
+import React, { createContext } from 'react'
+import { food_list } from '../assets/assets'
 
-export const StoreProvider = (props) => {
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
     const contextValue = {
-
+        food_list
     }
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
 }
+
 export default StoreContextProvider
