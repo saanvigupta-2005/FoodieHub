d275f3f393cfbfbd3bf8c62e2285b0b63dc17226
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-12 15:55:38 +0530

    changes till 2:35

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -15,6 +15,23 @@ const LoginPopup = ({ setShowLogin }) => {
           <h2>{currState}</h2>
           <img onClick={()=> setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
+        <div className="login-popup-inputs">
+            {currState === "Login" ? <></>:<input type="text" placeholder='Your Name' required/>}
+
+            <input type="email" placeholder='Your Email' required/>
+            <input type="password" placeholder='Password' required/>
+        </div>
+        <button>{currState=== "Sign Up" ? "Create Account" : "Login"}</button>
+        <div className="login-popup-condition">
+            <input type="checkbox"  required/>
+            <p>By continuing, I agree to the Terms of Service and Privacy Policy.</p>
+        </div>
+        {
+            currState==="Login"
+            ?<p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
+            :<p>Already have an account? <span onClick={()=> setCurrState("Login")}>Login here</span></p>
+        }
+        
       </form>
     </div>
   )
