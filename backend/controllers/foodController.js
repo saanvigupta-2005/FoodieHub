aa6524d35280c2c4149ccec6aaea48abd29e3219
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-15 23:10:28 +0530

    4:17

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -19,8 +19,20 @@ const addFood = async (req, res) => {
         res.json({success:false,message:"Error"})
 
     }
-
-
 };
 
-export {addFood}
+//all food list
+const listFood = async(req,res)=>{
+    try{
+        const foods = await foodModel.find({});
+        res.json({success:true,data:foods})
+
+    }catch(error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+
+    }
+
+}
+
+export {addFood,listFood}
