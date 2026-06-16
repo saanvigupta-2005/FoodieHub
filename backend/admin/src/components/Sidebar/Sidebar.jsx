34ca059645c548c2c8d31cf75242a0bb7b3be2cc
shiksha101 <shikshadahiya1710@gmail.com>
2026-06-16 16:54:31 +0530

    4:47

--- backend/admin/src/components/Sidebar/Sidebar.jsx
+++ backend/admin/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import  './Sidebar.css'
-const Sidebar =()=>{
-    return(
-        <div>
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-options'>
+
+        <div className='sidebar-option'>
+          <img src={assets.add_icon} alt='' />
+          <p>Add Items</p>
         </div>
-    )
+
+        <div className='sidebar-option'>
+          <img src={assets.order_icon} alt='' />
+          <p>List Items</p>
+        </div>
+
+        <div className='sidebar-option'>
+          <img src={assets.parcel_icon} alt='' />
+          <p>Orders</p>
+        </div>
+
+      </div>
+    </div>
+  )
 }
+
 export default Sidebar
