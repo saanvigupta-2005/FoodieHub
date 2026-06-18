ea531b1f9de7ba17d62fb4b80c1ce649fb46597a
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-19 00:40:12 +0530

    6:47:54

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -2,8 +2,13 @@ import React,{useState,useEffect} from 'react'
 import './LoginPopup.css'
 
 import {assets} from '../../assets/assets'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
+import axios from "axios"
 
 const LoginPopup = ({ setShowLogin }) => {
+
+  const {url,setToken} = useContext(StoreContext)
 
     const[currState,setCurrState] = useState("Login")
     const[data,setData]= useState({
@@ -11,18 +16,39 @@ const LoginPopup = ({ setShowLogin }) => {
       email:"",
       password:""
     })
-     const onChangeHandler =(event)=>{
+    const onChangeHandler =(event)=>{
       const name = event.target.name;
       const value = event.target.value;
       setData(data=>({...data,[name]:value}))
+    }
+    const onLogin = async (event) => {
+      event.preventDefault() 
+      let newUrl = url;
+      if(currState==="Login"){
+        newUrl += "/api/user/login"
+      }
+      else{
+        newUrl += "/api/user/register"
+      }
+      const response = await axios.post(newUrl,data);
+      if(response.data.success){
+        setToken(response.data.token);
+        localStorage.setItem("token",response.data.token)
+        setShowLogin(false)
+      }
+      else{
+        alert(response.data.message)
+      }
 
-     }
+    }
+
+
      useEffect(()=>{
       console.log(data);
      },[data])
   return (
     <div className='login-popup'>
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <img onClick={()=> setShowLogin(false)} src={assets.cross_icon} alt="" />
@@ -33,7 +59,7 @@ const LoginPopup = ({ setShowLogin }) => {
             <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your Email' required/>
             <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required/>
         </div>
-        <button>{currState=== "Sign Up" ? "Create Account" : "Login"}</button>
+        <button type='submit'>{currState=== "Sign Up" ? "Create Account" : "Login"}</button>
         <div className="login-popup-condition">
             <input type="checkbox"  required/>
             <p>By continuing, I agree to the Terms of Service and Privacy Policy.</p>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({ setShowLogin }) => {
      const[menu,setMenu]= useState("menu");
-     const{getTotalCartAmount}= useContext(StoreContext);
+     const{getTotalCartAmount,token,setToken}= useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -23,7 +23,15 @@ const Navbar = ({ setShowLogin }) => {
                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
-            <button onClick={() => setShowLogin(true)}>sign in</button>
+            {!token? <button onClick={() => setShowLogin(true)}>sign in</button>
+            :<div className='navbar-profile'>
+                <img src={assets.profile_icon} alt="" />
+                <ul className='nav-profile-dropdown'>
+                    <li><img src = {assets.bag_icon} alt="" /><p>Orders</p></li>
+                    <hr />
+                    <li><img src = {assets.logout_icon} alt="" /><p>Logout</p></li>
+                </ul>
+            </div>}
         </div>
       
     </div>
