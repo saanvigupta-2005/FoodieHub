a0ce949319ea5d8db87bf10bf55be9956d582c5d
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-20 00:48:53 +0530

    7:29:10

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import jwt from JsonWebTokenError
+
+const authMiddleware = async(req,res,next)=>{
+    const {token} = req.headers;
+    if(!token){
+        return res.json({success:false,message:'Not Authorized Login Again'})
+    }
+    try{
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userId = token_decode.id;
+    }catch(error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+
+    }
+
+
+}
+export default authMiddleware
