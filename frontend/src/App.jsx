a74e20f80d2e36803ffd9e775438276e2334cdf8
shiksha101 <shikshadahiya1710@gmail.com>
2026-06-21 19:21:04 +0530

    Stripe integration and payment verification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import {useState} from 'react'
-import LoginPopup from './components/LoginPopup/LoginPopup'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Verify from './pages/Verify/Verify';
+import MyOrders from './pages/MyOrders/MyOrders';
+import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = () => {
   const [showLogin, setShowLogin] = React.useState(false);
@@ -19,6 +20,9 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/order' element={<PlaceOrder />} />
+        <Route path='/verify' element={<verify/>}/>
+         <Route path='/myorders' element={<MyOrders/>}/>
+
       </Routes>
       
     </div>
