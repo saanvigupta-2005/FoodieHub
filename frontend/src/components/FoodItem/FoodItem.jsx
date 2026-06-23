32582a5081408ccc1651346db4c698499a631bc5
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-23 17:08:10 +0530

    change

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -7,12 +7,14 @@ import { StoreContext } from '../../context/StoreContext'
 const FoodItem = ({id, name, price, description,image}) => {
 
     const {cartItems,addToCart, removeFromCart,url} = useContext(StoreContext)
+    console.log("cartItems:", cartItems);
+    console.log("id:", id);
 
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className="food-item-image" src={url+"/images/"+image} alt="" />
-            {!cartItems[id]
+            {!cartItems?.[id]
                 ?<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
                 :<div className='food-item-counter'>
                     <img src={assets.remove_icon_red} alt="" onClick={() => removeFromCart(id)} />
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -81,7 +81,7 @@ const StoreContextProvider = (props) => {
   };
   const loadCartData= async (token)=>{
     const response =await axios.post(url+"/api/cart/get",{},{headers:{token}});
-    setCartItems(response.data.cartData);
+    setCartItems(response.data.cartData || {});
   }
 
   useEffect(() => {
