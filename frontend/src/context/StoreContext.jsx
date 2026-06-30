48f9f208cddadfa8ff4aa9a399940ba50e0ff39a
saanvigupta-2005 <saanvi167btcse23@igdtuw.ac.in>
2026-06-30 16:55:25 +0530

    changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-  const url = "http://localhost:4000";
+  const url = "https://foodie-hub-sage.vercel.app/";
 
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
